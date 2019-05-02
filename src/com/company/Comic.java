405703a49ad78677afbbcc3dd922bc201a0df70a
jy8474np <alksdjfasl;dkfjasdl;kj>
2019-05-01 19:13:42 -0500

    Comic book project in progress: Main and Comic classes created.

--- src/com/company/Comic.java
+++ src/com/company/Comic.java
@@ -4,17 +4,18 @@ public class Comic {
     protected int collectorNo;
     protected String bookTitle;
     protected int issueNo;
-    protected  int year;
     protected  String issueTitle;
+    protected  int year;
     protected double price;
     protected double value;
 
-    public Comic(int collectorNo, String bookTitle, int issueNo, int year, String issueTitle, double price, double value) {
+    public Comic(int collectorNo, String bookTitle, int issueNo,
+                 String issueTitle, int year, double price, double value) {
         this.collectorNo = collectorNo;
         this.bookTitle = bookTitle;
         this.issueNo = issueNo;
-        this.year = year;
         this.issueTitle = issueTitle;
+        this.year = year;
         this.price = price;
         this.value = value;
     }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,29 +6,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-        ArrayList<Comic> allComics = new ArrayList<Comic>();
+        ArrayList<Comic> allComics = new ArrayList<>();
 
-        Comic exampleComic = new Comic(999, "Batman", 321, 1939, "The Batman", 0.10, 1000000.00);
+        Comic exampleComic = new Comic(987, "Batman", 123, "The Batman",
+                1939, 0.10, 1000000.00);
 
         allComics.add(exampleComic);
-
-        ArrayList<Album> allInventory = new ArrayList<>();
-        allInventory.addAll(lpInventory);
-        allInventory.addAll(cdInventory);
-
-        String searchText = "Batman"; // ("Enter artist or title to search for. Works for partial artists/titles.");
-        searchforComic(allComics), searchText);
-
-        searchText = searchText.toLowerCase();
-        boolean found = false;
-        for (Comic c : allInventory) {
-            if (c.match(searchText)) {
-                found = true;
-                System.out.println(c);
-            }
-        }
-        if (!found) {
-            System.out.println("No matching comics found for" + searchText);
-        }
     }
 }
